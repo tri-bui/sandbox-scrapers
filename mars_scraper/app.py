91b818149a5810c651403eab2b45512dfb207d40
tri-bui <buitri91@gmail.com>
2021-02-20 12:00:48 -0800

    Update scrape route in mars_scraper/app.py

--- mars_scraper/app.py
+++ mars_scraper/app.py
@@ -7,7 +7,8 @@ import mars_scraper
 app = Flask(__name__)
 
 # App's connection to MongoDB
-mongo_uri = 'mongodb://localhost:27017/mars_db'
+db_name = 'mars_db'
+mongo_uri = 'mongodb://localhost:27017/' + db_name
 app.config['MONGO_URI'] = mongo_uri
 mongo = PyMongo(app)
 
@@ -32,8 +33,9 @@ def scrape():
 
     mars = mongo.db.mars # Mars collection
     mars_data = mars_scraper.scrape_all() # scrape new data
-    mars.replace_one({}, mars_data, upsert=True) # update mars collection with new data
-    return index() # render home page
+    mars.update({}, mars_data, upsert=True) # create or update mars collection with new data
+    # return index() # render home page
+    return redirect('/', code=302) # redirect to home page with updated data
 
 
 if __name__ == '__main__':
