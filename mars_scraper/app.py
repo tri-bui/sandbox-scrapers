7778accab28112b47a65c477f4d29986e9f1dfc8
tri-bui <buitri91@gmail.com>
2021-02-20 11:42:17 -0800

    Update comments in mars_scraper/app.py

--- mars_scraper/app.py
+++ mars_scraper/app.py
@@ -6,27 +6,34 @@ import mars_scraper
 # Flask app
 app = Flask(__name__)
 
-# Flask app's connection to MongoDB
+# App's connection to MongoDB
 mongo_uri = 'mongodb://localhost:27017/mars_db'
 app.config['MONGO_URI'] = mongo_uri
 mongo = PyMongo(app)
 
 
-""" Routes """
+""" App Routes """
 
 
 @app.route('/')
 def index():
-    mars_doc = mongo.db.mars.find_one() # Document containing Mars data
-    return render_template('index.html', mars=mars_doc)
+
+    """ Home page displaying all data """
+
+    # Extract Mars data and render home page
+    mars_doc = mongo.db.mars.find_one() # 1st document in mars collection contains all data
+    return render_template('index.html', mars=mars_doc) # pass data to and render home page
 
 
 @app.route('/scrape')
 def scrape():
+
+    """ Scrape new data, update database with new data, and refresh page """
+
     mars = mongo.db.mars # Mars collection
-    mars_data = nasa_scraper.scrape_all() # Scrape NASA site
-    mars.replace_one({}, mars_data, upsert=True) # Update data
-    return index()
+    mars_data = mars_scraper.scrape_all() # scrape new data
+    mars.replace_one({}, mars_data, upsert=True) # update mars collection with new data
+    return index() # render home page
 
 
 if __name__ == '__main__':
