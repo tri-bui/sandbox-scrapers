b2c8c56c820be628263f24ba9d693ff895de4625
tri-bui <buitri91@gmail.com>
2021-02-23 20:29:17 -0800

    Change db back to mars_db in app.py

--- mars_scraper/app.py
+++ mars_scraper/app.py
@@ -7,7 +7,7 @@ import mars_scraper
 app = Flask(__name__)
 
 # App's connection to MongoDB
-db_name = 'mars'
+db_name = 'mars_db'
 mongo_uri = 'mongodb://localhost:27017/' + db_name
 app.config['MONGO_URI'] = mongo_uri
 mongo = PyMongo(app)
